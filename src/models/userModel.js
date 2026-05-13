168e7d0ac7173969451661748d719282e67b7a87
Arshdeep Singh <arshdeepchhabra1712@gmail.com>
2026-05-13 13:50:18 +0530

    committed minor changes in userModel

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -9,11 +9,17 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, "Email is required"],
-        unique: [true, "Email must be unique"]
+        unique: [true, "Email must be unique"],
+        match: [
+            /^[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,63}$/,
+            "Please enter a valid email address"
+        ]
+
     },
     password: {
         type: String,
         required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"]
     },
     role: {
         type: String,
